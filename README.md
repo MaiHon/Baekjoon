477ad9eba63abaa193d8011249a31dab4e1db0b0
HongYeob Kim <rlaghdduq1130@gmail.com>
2018-10-07 14:41:14 +0900

    Solved BJ9012 by Stack

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Baekjoon
 -----
-[9498](https://boj.kr/9498) : [BJ9498.java](src/BJ9498.java) : 2018-10-07
+[9012](https://boj.kr/9012) : [BJ9012.java](src/BJ9012.java) : 2018-10-07
 
-- 개잘품
-- 알고짱짱맨
+- Stack
+- Feat 갓효원
---
+++ src/BJ9012.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Stack;
+
+public class BJ9012 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testNum = Integer.parseInt(br.readLine());
+        for(int i=0; i<testNum; i++){
+            Stack<Character> ch = new Stack();
+            char[] input = br.readLine().toCharArray();
+            boolean isEnd = false;
+            for(int j=0; j<input.length; j++) {
+                if(input[j]=='(') ch.push(input[j]);
+                else if(input[j]==')' && !ch.isEmpty()) ch.pop();
+                else if(input[j]==')' && ch.isEmpty()) {
+                    bw.write("NO\n");
+                    isEnd = true;
+                    break;
+                }
+            }
+            if (!isEnd) {
+                if (!ch.isEmpty()) {
+                    bw.write("NO\n");
+                } else {
+                    bw.write("YES\n");
+                }
+            }
+        }
+        bw.flush();
+    }
+}
