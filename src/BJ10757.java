8691a99c56b3fe1fd0763ce998c7825cb4d21c5f
HongYeob Kim <rlaghdduq1130@gmail.com>
2019-01-16 19:13:50 +0900

    2019-01-16

---
+++ src/BJ10757.java
@@ -0,0 +1,14 @@
+import java.util.*;
+        import java.math.*;
+
+public class BJ10757 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        BigInteger a, b ;
+
+        a = sc.nextBigInteger();
+        b = sc.nextBigInteger();
+
+        System.out.println(a.add(b));
+    }
+}
